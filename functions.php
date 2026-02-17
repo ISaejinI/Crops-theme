42f5704d4d83e999f88b51284e0f1ee6dc49a509
Saejin <saejinmx@gmail.com>
2026-02-17 14:34:24 +0100

    Add custom login styles

--- functions.php
+++ functions.php
@@ -3,6 +3,15 @@ add_filter('sanitize_file_name', 'remove_accents');
 
 function crops_register_assets()
 {
-  wp_enqueue_style('main', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
+    wp_enqueue_style('main', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'crops_register_assets');
+
+function crops_login_logo() {
+    wp_enqueue_style(
+        "crops-login",
+        get_template_directory_uri() . "/assets/css/login.css",
+        array('login')
+    );
+}
+add_action('login_enqueue_scripts', 'crops_login_logo');
